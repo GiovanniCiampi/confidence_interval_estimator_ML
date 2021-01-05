728838ce13f7c9f2d03188e66f50c0f913007342
root <giovanniciampi95@gmail.com>
2021-01-05 11:13:25 +0000

    fixed print of percentages

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
 # __init__.py
 
 # Version of the Bootstrap_Confidence_Intervals_ML package
-#__version__ = "1.0.0"
+__version__ = "0.1.2"
--- utils.py
+++ utils.py
@@ -43,10 +43,10 @@ def parametric_confidence_interval(confidence_level, mean, sample_size, verbose=
     r = mean-lower_bound
     
     if verbose: 
-        rounded_confidence_level = round(confidence_level, 3)
+        rounded_confidence_level = round(confidence_level*100, 3)
         rounded_r = round(r, 3)
-        rounded_mean = round(mean*100, 3)
-        print(f"With {rounded_confidence_level}% confidence the accuracy of the model is in {rounded_mean}% +/-{rounded_r}.")
+        rounded_mean = round(mean, 3)
+        print(f"With {rounded_confidence_level}% confidence the accuracy of the model is in {rounded_mean} +/-{rounded_r}.")
     return mean, r
 
 def get_accuracy_samples(instantiable_model, X, y, model_params_dict=None,
@@ -183,11 +183,11 @@ def nonparametric_confidence_interval(acc_dict, confidence_level, verbose=True, 
     r = mean - lower_bound
     
     if verbose:
-        rounded_mean = round(mean*100, 3)
-        rounded_r = round(r*100, 3)
+        rounded_mean = round(mean, 3)
+        rounded_r = round(r, 3)
         rounded_confidence = round(confidence_level*100, 3)
         
-        str_accuracy = f"{rounded_mean}% +/-{rounded_r}"
+        str_accuracy = f"{rounded_mean} +/-{rounded_r}"
         print(f"From the given data, with {rounded_confidence}% confidence the accuracy of the model is in {str_accuracy}.")
     
     if return_median:
